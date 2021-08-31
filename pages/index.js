aaf973c2e50536c9e6116d3da18ce7ec5fee9eaa
Evandro oliveira <vandoaparecido@hotmail.com>
2021-08-31 00:02:18 -0300

    :rocket: Validando campos com yup e formik

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,32 @@ import {
   FormControl,
   FormLabel,
   FormHelperText,
-  Flex,
 } from "@chakra-ui/react";
+import { useEffect } from "react";
+import * as yup from "yup";
+import { useFormik } from "formik";
 import { Logo } from "../components";
 
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("E-mail inválido")
+    .required("Preenchimento obrigatório"),
+
+  password: yup.string().required("Preenchimento obrigatório"),
+  username: yup.string().required("Preenchimento obrigatório"),
+});
+
 export default function Home() {
+  const { values, errors, touched, handleBlur, handleChange } = useFormik({
+    onSubmit: () => {},
+    validationSchema,
+    initialValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+  });
   return (
     <Container p={4} centerContent>
       <Logo />
@@ -23,18 +44,50 @@ export default function Home() {
       <Box>
         <FormControl id="email" p={4} isRequired>
           <FormLabel>Email</FormLabel>
-          <Input size="lg" type="email" />
+          <Input
+            size="lg"
+            type="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+
+          {touched.email && (
+            <FormHelperText textColor="#e74c3c">{errors.email}</FormHelperText>
+          )}
         </FormControl>
 
         <FormControl id="password" p={4} isRequired>
           <FormLabel>Senha</FormLabel>
-          <Input size="lg" type="password" />
+          <Input
+            size="lg"
+            type="password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+
+          {touched.password && (
+            <FormHelperText textColor="#e74c3c">
+              {errors.password}
+            </FormHelperText>
+          )}
         </FormControl>
 
         <Box display="Flex" flexDirection="row" alignItems="center">
           <Text>Cloker.work</Text>
           <FormControl id="username" p={4} isRequired>
-            <Input type="username" />
+            <Input
+              type="username"
+              value={values.username}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {touched.username && (
+              <FormHelperText textColor="#e74c3c">
+                {errors.username}
+              </FormHelperText>
+            )}
           </FormControl>
         </Box>
 
