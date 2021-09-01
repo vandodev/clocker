beab623bcf95f3e1ebecfac243e0f3afea616e0a
Evandro oliveira <vandoaparecido@hotmail.com>
2021-09-01 01:17:06 -0300

    :rocket: Redirecionando usuário logado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,32 @@
-import { Login } from "../components";
+import { Agenda, Login } from "../components";
+import { useEffect, useState } from "react";
+import firebase from "../config/firebase";
+import { Spinner, Container } from "@chakra-ui/react";
 
 export default function Home() {
-  return <Login />;
+  // const authenticateUser = firebase.auth().currentUser;
+  const [auth, setAuth] = useState({
+    loading: true,
+    user: false,
+  });
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setAuth({
+        loading: false,
+        user: !!user,
+      });
+    });
+  }, []);
+
+  if (auth.loading) {
+    // return "Loading ...";
+    return (
+      <Container p={4} centerContent>
+        <Spinner />
+      </Container>
+    );
+  }
+
+  return auth.user ? <Agenda /> : <Login />;
 }
